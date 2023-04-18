86503e21d02234a64de2a4be7522f1656dbdeece
ShivajiNimbalkar <121384303+ShivajiNimbalkar@users.noreply.github.com>
2023-04-19 03:41:57 +0530

    Add files via upload
    SQL Dataset Script

---
+++ Financial Dataset.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [type]
+      ,[amount]
+      ,[Organization_ID]
+      ,[oldbalanceOrg]
+      ,[newbalanceOrig]
+      ,[nameDest]
+      ,[oldbalanceDest]
+      ,[newbalanceDest]
+      ,[isFraud]
+  FROM [FRAUD_COSTOMER_ANALYSIS].[dbo].[Financial Dataset]
